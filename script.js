d43bfd6ef0d3c271dead43a075dbee46d2249656
sateda <7441698+sateda@users.noreply.github.com>
2025-02-16 14:42:15 +0100

    Complete function playRound

--- script.js
+++ script.js
@@ -8,18 +8,19 @@ function getComputerChoice() {
     // Define computer choice
     switch(getRandomInteger) {
         case 0:
-            computerChoice = "Rock";
+            computerChoice = "rock";
             break;
         case 1:
-            computerChoice = "Paper";
+            computerChoice = "paper";
             break;
         case 2:
-            computerChoice = "Scissors";
+            computerChoice = "scissors";
             break;
         default:
             console.log(`Error computerChoice: ${getRandomInteger}`);
     }
 
+    // Return value of computerChoice in lowercase
     return computerChoice;
 }
 
@@ -38,8 +39,43 @@ function getHumanChoice () {
 }
 
 // Function for one round takes two arguments to compare and decides the winner
-function playRound(humanChoice, computerChoice) {
+function playRound(humanChoice, computerChoice) {  
+    console.log("You're choice: " + humanChoice); // display you're choice
+    console.log("Computers choice: " + computerChoice); // display computers choice
 
+    // Check who wins
+    switch(true) {
+        case (humanChoice === computerChoice):
+            console.log("This is a tie, try again")
+            playRound(getHumanChoice(), getComputerChoice()); // play again
+            break;
+        case (humanChoice === "rock" && computerChoice === "paper"):
+            console.log("You lose, paper beat rock!");
+            computerScore++;
+            break;
+        case (humanChoice === "rock" && computerChoice === "scissors"):
+            console.log("You win! rock beats scissors.");
+            humanScore++;
+            break;
+        case (humanChoice === "paper" && computerChoice === "scissors"):
+            console.log("You lose, scissors beat paper!");
+            computerScore++;
+            break;
+        case (humanChoice === "paper" && computerChoice === "rock"):
+            console.log("You win! paper beats rock.");
+            humanScore++;
+            break;
+        case (humanChoice === "scissors" && computerChoice === "rock"):
+            console.log("You lose, rock beat scissors!");
+            computerScore++;
+            break;
+        case (humanChoice === "scissors" && computerChoice === "paper"):
+            console.log("You win! scissors beats paper.");
+            humanScore++;
+            break;
+        default:
+            console.log("some error occurred?");
+    }
 }
 
 // Function for one game, has five rounds and declares a winner
